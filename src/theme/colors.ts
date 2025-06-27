fdd2a67c8f6a5e432b0275da8dc747168efcb6fc
dakariwrenn <dakari1.wrenn@famu.edu>
2025-06-27 19:30:39 -0400

    Create colors.ts

---
+++ src/theme/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  background: '#222428',
+  card: '#272B30',
+  accent: '#6EE2F5',      // metallic blue
+  metallic: '#A7A9AC',    // soft metallic
+  text: '#F3F3F3',
+  secondaryText: '#B9BBBE',
+  upvote: '#4ADE80',      // green
+  downvote: '#F87171',    // red
+  event: '#F6C177',       // warm gold for events
+};
+export default colors;
